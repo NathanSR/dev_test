ca0b09388e9be954c76401d2a23652e0368b3eaf
Nathan Santos <nathansr.nsr@gmail.com>
2024-11-16 21:01:47 -0300

    Projeto testado com sucesso - NathanSR

--- init.sql
+++ init.sql
@@ -1,5 +1,18 @@
 USE test_db;
 
 --TODO Crie a tabela de user;
+CREATE TABLE IF NOT EXISTS user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
 
 --TODO Crie a tabela de posts;
+CREATE TABLE IF NOT EXISTS post (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    userId INTEGER NOT NULL,
+    FOREIGN KEY (userId) REFERENCES user(id)
+);
--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const AppDataSource = new DataSource({
   username: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "password",
   database: process.env.DB_NAME || "test_db",
-  entities: [User,Post],
+  entities: [User, Post],
   synchronize: true,
 });
 
@@ -33,13 +33,62 @@ const initializeDatabase = async () => {
 
 initializeDatabase();
 
+const UserDB = AppDataSource.getRepository(User)
+const PostDB = AppDataSource.getRepository(Post)
+
 app.post('/users', async (req, res) => {
-// Crie o endpoint de users
+  try {
+    const { firstName, lastName, email } = req.body;
+    if (!firstName) return res.status(400).json({ message: "Nome não informado!" })
+    if (!lastName) return res.status(400).json({ message: "Sobrenome não informado!" })
+    if (!email) return res.status(400).json({ message: "Email não informado!" })
+
+    const user = new User()
+    user.email = email;
+    user.firstName = firstName;
+    user.lastName = lastName;
+
+    await UserDB.save(user)
+    res.status(201).json(user)
+
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao salvar o usuário!" })
+  }
 });
 
+// app.get('/users', async (req, res) => {
+//   const users = await UserDB.find()
+//   res.status(200).json(users)
+// })
+
 app.post('/posts', async (req, res) => {
-// Crie o endpoint de posts
+  try {
+    const { title, description, userId } = req.body;
+    if (!title) return res.status(400).json({ message: "Título não informado!" })
+    if (!description) return res.status(400).json({ message: "Descrição não informada!" })
+    if (!userId) return res.status(400).json({ message: "Usuário não informado!" })
+
+    const post = new Post()
+    post.title = title;
+    post.description = description;
+
+    const user = await UserDB.findOneBy({ id: userId })
+    if (!user) return res.status(404).json({ message: "Usuário informado não foi encontrado!" });
+
+    post.user = user;
+
+    await PostDB.save(post);
+    res.status(201).json(post);
+
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao salvar o post" })
+  }
 });
+
+// app.get('/posts', async (req, res) => {
+//   const posts = await PostDB.find({ relations: ['user'] })
+//   res.status(200).json(posts)
+// })
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
